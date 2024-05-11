c5b35663effb69318ed6541ac87c152f13519caf
janchovanec <jan.chovanec01@gmail.com>
2024-05-11 16:51:40 +0200

    FlappyClone finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import {GameComponent} from "./game/game.component";
+import {MenuComponent} from "./menu/menu.component";
+import {SettingsComponent} from "./settings/settings.component";
+import {HighscoresComponent} from "./highscores/highscores.component";
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
+  { path: 'game', component: GameComponent },
+  { path: 'menu', component: MenuComponent},
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'highscores', component: HighscoresComponent },
 ];
